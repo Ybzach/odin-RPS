f33438408899ff23d32e68b35aa927ade8aa852d
YBZach <boonzhe.yaw@gmail.com>
2021-12-27 19:50:03 +0800

    Game is fully interactable

--- script.js
+++ script.js
@@ -4,39 +4,51 @@ function computerPlay() {
 }
 
 function playRound(e) {
-    // playerSelection = player.toLowerCase();
+    const playerSelection = (e.target.innerText).toLowerCase();
+    const computerChoice = computerPlay();
     
-    // if (playerSelection === "rock"){
-    //     if (CPU === "scissors") {
-    //         return "player";
-    //     } else if (CPU === "paper"){
-    //         return "CPU";
-    //     } else return "tie";
-    // }else if (playerSelection === "paper"){
-    //     if (CPU === "rock") {
-    //         return "player";
-    //     } else if (CPU === "scissors"){
-    //         return "CPU";
-    //     } else return "tie";
-    // }else if (playerSelection === "scissors"){
-    //     if (CPU === "paper") {
-    //         return "player";
-    //     } else if (CPU === "rock"){
-    //         return "CPU";
-    //     } else return "tie";
-    // }
+    console.log(playerSelection);
+    console.log(computerChoice);
+    if (playerSelection === "rock"){
+        if (computerChoice === "scissors") {
+            return "player";
+        } else if (computerChoice === "paper"){
+            return "CPU";
+        } else return "tie";
+    }else if (playerSelection === "paper"){
+        if (computerChoice === "rock") {
+            return "player";
+        } else if (computerChoice === "scissors"){
+            return "CPU";
+        } else return "tie";
+    }else if (playerSelection === "scissors"){
+        if (computerChoice === "paper") {
+            return "player";
+        } else if (computerChoice === "rock"){
+            return "CPU";
+        } else return "tie";
+    }
 }
 
-function getMove(e) {
-    const move = e.target.innerText;
+function updateCounter(e){
+    const winner = playRound(e);
+    const playerScoreDiv = document.querySelector(".player-score");
+    const CPUScoreDiv = document.querySelector(".CPU-score");
+    let playerScore = parseInt(playerScoreDiv.innerText);
+    let CPUScore = parseInt(CPUScoreDiv.innerText); 
 
+    if (winner === "player"){
+        playerScore++;
+        playerScoreDiv.textContent = `${playerScore}`;
+    } else if (winner === "CPU") {
+        CPUScore++;
+        CPUScoreDiv.textContent = `${CPUScore}`;
+    }
 }
 
 function main(){
     const buttons = document.querySelectorAll("#choice-buttons button");
-    const computerChoice = computerPlay();
 
-    buttons.forEach(button => button.addEventListener("click",getMove));
+    buttons.forEach(button => button.addEventListener("click",updateCounter));
 }
 main();
-// game();
